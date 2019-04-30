c428068c1115dffe70b43410ebd71806dc0740e9
xy <13122156670@163.com>
2019-04-30 11:08:20 +0800

    update

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('*', function (req, res) {
 // 监听
 console.log('服务器开启中，请稍后...');
 app.listen(8080, function () {
-    console.log(`请访问http://127.0.0.1:8080 端口进行 %d 项目预览`);
+    console.log(`请访问http://127.0.0.1:8080 端口进行项目查看预览`);
     console.log(`cmd按Ctrl + c 退出服务`);
     console.log(`或者输入netstat -ano | findstr 8080查看端口是否存在`);
     console.log(`如果存在输入taskkill -PID 请把这段替换为8080跟的进程号：例如127.0.0.1:8080 0.0.0.0 某某 11424 -F`);
